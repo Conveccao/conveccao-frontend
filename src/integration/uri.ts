c3a51531aec3a7bb5ceea7ec284f4f71a445c268
gustavols <gustavoleandro616@gmail.com>
2022-10-17 20:38:31 -0300

    Fix: Alterado questões de rotas

--- src/integration/uri.ts
+++ src/integration/uri.ts
@@ -1,10 +1,10 @@
 export enum URI{
-    STATIONS = 'http://localhost:8080/stations',
-    PARAMETERS = 'http://localhost:8080/parameters',
-    PARAMETERTYPES = 'http://localhost:8080/parameterTypes',
-    CREATEUSER = 'http://localhost:8080/user',
-    USEREXISTS = 'http://localhost:8080/user-exists',
-    USERS = 'http://localhost:8080/users',
-    UPDATEUSER = "http://localhost:8080/update-user",
-    LOGIN = 'http://localhost:8080/login'
+    STATIONS = 'http://localhost:8081/stations',
+    PARAMETERS = 'http://localhost:8081/parameters',
+    PARAMETERTYPES = 'http://localhost:8081/parameterTypes',
+    CREATEUSER = 'http://localhost:8081/user',
+    USEREXISTS = 'http://localhost:8081/user-exists',
+    USERS = 'http://localhost:8081/users',
+    UPDATEUSER = "http://localhost:8081/update-user",
+    LOGIN = 'http://localhost:8081/login'
 }
