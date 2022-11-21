ffc61f1dbdca7451a2f57d644c74b6a8e8cb721a
Gustavo Santos <55259166+gustavols@users.noreply.github.com>
2022-11-21 07:56:00 -0300

    Update axios.ts

--- src/integration/axios.ts
+++ src/integration/axios.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-    //baseURL: "https://conveccao.onrender.com"
-    baseURL: "http://localhost:8081" // -> Para backend local
+    baseURL: "http://174.138.61.49:80"
+    //baseURL: "http://localhost:8081" // -> Para backend local
 })
