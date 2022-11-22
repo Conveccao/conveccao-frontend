531516748a80f9ae74147bee358ffccf2f81e6b0
Gustavo Santos <55259166+gustavols@users.noreply.github.com>
2022-11-22 07:55:38 -0300

    Update uri.ts

--- src/integration/uri.ts
+++ src/integration/uri.ts
@@ -1,16 +1,16 @@
 export enum URI{
-    STATIONS = 'http://174.138.61.49:80/stations',
-    PARAMETERS = 'http://174.138.61.49:80/parameters',
-    PARAMETERTYPES = 'http://174.138.61.49:80/parameterTypes',
-    CREATEUSER = 'http://174.138.61.49:80/user',
-    USEREXISTS = 'http://174.138.61.49:80/user-exists',
-    USERS = 'http://174.138.61.49:80/users',
-    UPDATEUSER = "http://174.138.61.49:80/update-user",
-    LOGIN = 'http://174.138.61.49:80/login',
-    MEASURES = 'http://174.138.61.49:80/measures',
-    STATIONMEASURES = 'http://174.138.61.49:80/station-measures',
-    DOWNLOADMEASURES = 'http://174.138.61.49:80/build-csv',
-    STATIONS_ACTIVE = 'http://174.138.61.49:80/active-stations',
-    ALERTS = 'http://174.138.61.49:80/alerts'
+    STATIONS = 'http://localhost:8081/stations',
+    PARAMETERS = 'http://localhost:8081/parameters',
+    PARAMETERTYPES = 'http://localhost:8081/parameterTypes',
+    CREATEUSER = 'http://localhost:8081/user',
+    USEREXISTS = 'http://localhost:8081/user-exists',
+    USERS = 'http://localhost:8081/users',
+    UPDATEUSER = "http://localhost:8081/update-user",
+    LOGIN = 'http://localhost:8081/login',
+    MEASURES = 'http://localhost:8081/measures',
+    STATIONMEASURES = 'http://localhost:8081/station-measures',
+    DOWNLOADMEASURES = 'http://localhost:8081/build-csv',
+    STATIONS_ACTIVE = 'http://localhost:8081/active-stations',
+    ALERTS = 'http://localhost:8081/alerts'
 }
 
